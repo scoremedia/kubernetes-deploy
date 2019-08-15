80ad2d822adc8206cdfbcace10ca9da32434211c
timothysmith0609 <tim.smith@shopify.com>
2019-08-15 13:47:18 -0400

    TemplateDiscovery cleanup

--- lib/kubernetes-deploy/template_discovery.rb
+++ lib/kubernetes-deploy/template_discovery.rb
@@ -2,35 +2,35 @@
 module KubernetesDeploy
   class TemplateDiscovery
     class << self
-      def templates(template_args)
-        template_paths = {}
-        dir_args = template_args.select { |arg| File.directory?(arg) }
-        file_args = template_args.select { |arg| File.file?(arg) }
+      def templates(template_paths)
+        templates = {}
+        dir_paths = template_paths.select { |path| File.directory?(path) }
+        file_paths = template_paths.select { |path| File.file?(path) }
 
-        # Directory arg
-        dir_args.each_with_object(template_paths) do |template_dir, hash|
+        # Directory paths
+        dir_paths.each_with_object(templates) do |template_dir, hash|
           hash[template_dir] = Dir.foreach(template_dir).select do |filename|
             filename.end_with?(".yml.erb", ".yml", ".yaml", ".yaml.erb")
           end
         end
-        # Filename arg
-        file_args.each_with_object(template_paths) do |filename, hash|
+        # Filename paths
+        file_paths.each_with_object(templates) do |filename, hash|
           dir_name = File.dirname(filename)
           hash[dir_name] ||= []
           hash[dir_name] << File.basename(filename) unless hash[dir_name].include?(filename)
         end
 
-        template_paths
+        templates
       end
 
-      def validate_templates(template_args)
+      def validate_templates(template_paths)
         file_regex = /(\.ya?ml(\.erb)?)$|(secrets\.ejson)$/
         errors = []
-        template_args.each do |arg|
-          if !File.directory?(arg) && !File.file?(arg)
-            errors << "Template does not exist. Couldn't find file or directory #{arg}"
-          elsif File.directory?(arg) && Dir.entries(arg).none? { |file| file =~ file_regex }
-            errors << "`#{arg}` doesn't contain valid templates (secrets.ejson or postfix .yml, .yml.erb)"
+        template_paths.each do |path|
+          if !File.directory?(path) && !File.file?(path)
+            errors << "Template does not exist. Couldn't find file or directory #{path}"
+          elsif File.directory?(path) && Dir.entries(path).none? { |file| file =~ file_regex }
+            errors << "`#{path}` doesn't contain valid templates (secrets.ejson or postfix .yml, .yml.erb)"
           end
         end
         errors
