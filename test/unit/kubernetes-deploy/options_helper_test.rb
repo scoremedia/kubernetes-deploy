c0a7848e7534a17d6b69e3775162689165589e76
timothysmith0609 <tim.smith@shopify.com>
2019-08-15 10:12:09 -0400

    optionshelper test + refactor

--- test/unit/kubernetes-deploy/options_helper_test.rb
+++ test/unit/kubernetes-deploy/options_helper_test.rb
@@ -35,37 +35,93 @@ class OptionsHelperTest < KubernetesDeploy::TestCase
     end
   end
 
-  def test_with_template_dir_from_stdin
-    old_stdin = $stdin
-    fixture_yamls = []
-    stdin_yamls = []
+  def test_with_multiple_template_dirs
+    KubernetesDeploy::OptionsHelper.with_validated_template_dirs([fixture_path('hello-cloud'), fixture_path('cronjobs')]) do |template_dirs|
+      assert_equal(template_dirs, [fixture_path('hello-cloud'), fixture_path('cronjobs')])
+    end
+  end
 
-    input = Tempfile.open("kubernetes_deploy_test")
-    fixture_path_entries = Dir.glob("#{fixture_path('hello-cloud')}/*.{yml,yaml}*")
-    fixture_path_entries.each do |filename|
+  def test_with_multiple_template_dirs_with_stdin
+    wrapped_stdin do |input|
+      fixture_yamls = []
+      stdin_yamls = []
+      fixture_yamls = fixtures_for_stdin(fixture_path: fixture_path("hello-cloud"), file: input)
+
+      KubernetesDeploy::OptionsHelper.with_validated_template_dirs(['-', 'cronjobs']) do |template_dirs|
+        stdin_dir = (template_dirs - ['cronjobs']).first
+        split_templates = File.read(
+          File.join(stdin_dir, KubernetesDeploy::OptionsHelper::STDIN_TEMP_FILE)
+        ).split(/^---$/).map(&:strip).reject(&:empty?)
+        refute(split_templates.empty?)
+        split_templates.each do |template|
+          stdin_yamls << YAML.safe_load(template)
+        end
+        fixture_yamls.each do |fixture|
+          assert(stdin_yamls.include?(fixture))
+        end
+        assert(template_dirs.include?('cronjobs'))
+      end
+    end
+  end
+
+  def test_with_template_dir_from_stdin
+    wrapped_stdin do |input|
+      fixture_yamls = []
+      stdin_yamls = []
+      fixture_yamls = fixtures_for_stdin(fixture_path: fixture_path("hello-cloud"), file: input)
+
+      KubernetesDeploy::OptionsHelper.with_validated_template_dirs(['-']) do |template_dirs|
+        split_templates = File.read(
+          File.join(template_dirs.first, KubernetesDeploy::OptionsHelper::STDIN_TEMP_FILE)
+        ).split(/^---$/).map(&:strip).reject(&:empty?)
+        refute(split_templates.empty?)
+        split_templates.each do |template|
+          stdin_yamls << YAML.safe_load(template)
+        end
+
+        fixture_yamls.each do |fixture|
+          assert(stdin_yamls.include?(fixture))
+        end
+      end
+    end
+  end
+
+  def test_with_repeated_template_dirs
+    wrapped_stdin do |input|
+      KubernetesDeploy::OptionsHelper.with_validated_template_dirs(['-', '-']) do |template_dirs|
+        assert_equal(template_dirs.length, 1)
+      end
+    end
+
+    KubernetesDeploy::OptionsHelper.with_validated_template_dirs([fixture_path('hello-cloud'), fixture_path('hello-cloud')]) do |template_dirs|
+      assert_equal(template_dirs.length, 1)
+    end
+  end
+
+  private
+
+  def fixtures_for_stdin(fixture_path:, file:)
+    fixture_yamls = []
+    fixture_path_entries = Dir.glob("#{fixture_path}/*.{yml,yaml}*")
+    fixture_path_entries.each_with_object(fixture_yamls) do |filename, fixtures|
       File.open(filename, 'r') do |f|
         contents = f.read
-        input.print(contents + "\n---\n")
-        contents.split(/^---$/).reject(&:empty?).each { |c| fixture_yamls << YAML.safe_load(c) }
+        file.print(contents + "\n---\n")
+        contents.split(/^---$/).reject(&:empty?).each { |c| fixtures << YAML.safe_load(c) }
       end
     end
-    input.rewind
+    file.rewind
+    fixture_yamls
+  end
+
+  def wrapped_stdin(&block)
+    old_stdin = $stdin
+    input = Tempfile.open("kubernetes_deploy_test")
     $stdin = input
-
-    KubernetesDeploy::OptionsHelper.with_validated_template_dirs(['-']) do |template_dirs|
-      split_templates = File.read(
-        File.join(template_dirs.first, KubernetesDeploy::OptionsHelper::STDIN_TEMP_FILE)
-      ).split(/^---$/).map(&:strip).reject(&:empty?)
-      refute(split_templates.empty?)
-      split_templates.each do |template|
-        stdin_yamls << YAML.safe_load(template)
-      end
-
-      fixture_yamls.each do |fixture|
-        assert(stdin_yamls.include?(fixture))
-      end
-    end
+    yield input
   ensure
     $stdin = old_stdin
+    input.close
+    input.unlink
   end
 end
